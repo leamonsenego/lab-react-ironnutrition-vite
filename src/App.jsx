a6e6e547768a2ed3230792bdd2f9e577494ff96e
leamonsenego <116014403+leamonsenego@users.noreply.github.com>
2023-10-19 18:41:32 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
 import "./App.css";
+import foodsJson from "./foods.json";
+import FoodBox from "./components/FoodBox";
+import AddFoodForm from "./components/AddFoodForm";
 
 function App() {
+  const [foods, setFoods] = useState(foodsJson);
+
+  function handleDeleteFood(indexToDelete) {
+    const updatedFoods = foods.filter((food, index) => index !== indexToDelete);
+    setFoods(updatedFoods);
+
+    return updatedFoods;
+  }
+
   return (
     <div className="App">
-      <h1>LAB | React IronNutrition</h1>
+      <h2>Add Food Entry</h2>
+      <AddFoodForm updateState={setFoods} />
+      <h1>Food list</h1>
+      {foods.map((food, index) => {
+        return (
+          <FoodBox
+            key={index}
+            food={food}
+            onClick={() => handleDeleteFood(index)}
+          />
+        );
+      })}
     </div>
   );
 }
