2f2789e01df62550a9479c301eb948ef42eddc33
leamonsenego <116014403+leamonsenego@users.noreply.github.com>
2023-10-19 18:42:09 +0200

    Update FoodBox.jsx

--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,23 @@
-// Your code here
+
+import React from "react";
+
+const FoodBox = (props)  => {
+  return (
+    <div>
+      <p>{props.food.name} </p>
+
+      <img src={props.food.image} style={{ width: "200px" }} />
+
+      <p>Calories: {props.food.calories}</p>
+      <p>Servings: {props.food.servings}</p>
+
+      <p>
+        <b>Total Calories: {props.food.calories * props.food.servings}</b> kcal
+      </p>
+
+      <button onClick={props.onClick}>Delete</button>
+    </div>
+  );
+}
+
+export default FoodBox
